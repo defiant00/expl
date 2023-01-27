64cc77751ab7f2b0b3adf17629b3eaf6b010c534
defiant00 <defiant00@gmail.com>
2023-01-27 10:24:09 -0800

    buffered output and version

--- src/main.zig
+++ src/main.zig
@@ -1,24 +1,22 @@
 const std = @import("std");
 
+const out = @import("out.zig");
+const version = @import("version.zig").version;
+
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    out.init();
+    defer out.flush();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    out.println("expl {d}.{d}.{d}", .{ version.major, version.minor, version.patch });
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    var allocator = gpa.allocator();
 
-    try bw.flush(); // don't forget to flush!
-}
+    var args = try std.process.argsWithAllocator(allocator);
+    defer args.deinit();
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+    var arg_list = std.ArrayList([]const u8).init(allocator);
+    defer arg_list.deinit();
+
+    while (args.next()) |arg| try arg_list.append(arg);
 }
---
+++ src/version.zig
@@ -0,0 +1,3 @@
+const Version = @import("std").builtin.Version;
+
+pub const version = Version{ .major = 0, .minor = 0, .patch = 1 };
